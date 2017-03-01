07d18fa7129bee5bcfff98f5ab422e489200036c
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-03-01 19:02:22 +0800

    add commit timing graph

--- render.py
+++ render.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
+import calendar
 
 from stats import stats
 
@@ -9,14 +10,25 @@ ENV = Environment(loader=FileSystemLoader(PATH + PREFIX, followlinks=True),
                   autoescape=select_autoescape(['html', 'xml']),
                   trim_blocks=True)
 
+ENV.globals['max'] = max
+ENV.globals['min'] = min
+
 
 def render_page(page, context):
-    return ENV.get_template(page).render(context)
+    return ENV.get_template('pages/{}.html'.format(page)).render(context)
 
 
-html = render_page('pages/projects.html', {
+def save_html(page, content):
+    with open('docs/{}.html'.format(page), encoding='utf-8', mode='w') as f:
+        f.write(content)
+
+# Render home
+save_html('index', render_page('projects', {
     'projects': stats.projects,
-})
+}))
 
-with open('docs/index.html', encoding='utf-8', mode='w') as f:
-    f.write(html)
+# Render graphs
+save_html('graphs', render_page('graphs', {
+    'commits': stats.analyze_commit_timing(),
+    'day_name': calendar.day_name
+}))
