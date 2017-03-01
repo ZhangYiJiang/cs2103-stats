47fdd7e091014cbecdfe974abd035306cc22b648
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-03-01 17:00:01 +0800

    add mockup wall

--- render.py
+++ render.py
@@ -1,7 +1,22 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import os
 
-env = Environment(
-    loader=PackageLoader('yourapplication', 'templates'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+from stats import stats
 
+PATH = os.path.dirname(os.path.abspath(__file__))
+PREFIX = '/templates'
+ENV = Environment(loader=FileSystemLoader(PATH + PREFIX, followlinks=True),
+                  autoescape=select_autoescape(['html', 'xml']),
+                  trim_blocks=True)
+
+
+def render_page(page, context):
+    return ENV.get_template(page).render(context)
+
+
+html = render_page('pages/projects.html', {
+    'projects': stats.projects,
+})
+
+with open('pages/index.html', encoding='utf-8', mode='w') as f:
+    f.write(html)
